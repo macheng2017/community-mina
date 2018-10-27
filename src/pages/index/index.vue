a579d00d843f8a85b0de517b6756078cc9e29b4d
mac <mac@macs-iMac.local>
2018-10-27 17:32:57 +0800

    添加pug sass 依赖,以及添加后端配置文件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -26,7 +26,7 @@
 import card from '@/components/card'
 
 export default {
-  data () {
+  data() {
     return {
       motto: 'Hello World',
       userInfo: {}
@@ -38,28 +38,28 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
+    bindViewTap() {
       const url = '../logs/main'
       wx.navigateTo({ url })
     },
-    getUserInfo () {
+    getUserInfo() {
       // 调用登录接口
       wx.login({
         success: () => {
           wx.getUserInfo({
-            success: (res) => {
+            success: res => {
               this.userInfo = res.userInfo
             }
           })
         }
       })
     },
-    clickHandle (msg, ev) {
+    clickHandle(msg, ev) {
       console.log('clickHandle:', msg, ev)
     }
   },
 
-  created () {
+  created() {
     // 调用应用实例的方法获取全局数据
     this.getUserInfo()
   }
